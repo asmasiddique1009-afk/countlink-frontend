9a3f33c9b5dbf278d589000eba5f423379947f62
asmasiddique1009-afk <asmasiddique1009@gmail.com>
2026-06-06 01:02:57 +0500

    Delete backend directory

--- backend/src/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({
-  conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  text: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', messageSchema);
--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getOrderStats, getRecentOrders } = require('../controllers/dashboardController');
-const { protect } = require("../middleware/authMiddleware");
-
-router.get('/stats', protect, getOrderStats);
-router.get('/recent-orders', protect, getRecentOrders);
-
-module.exports = router;
